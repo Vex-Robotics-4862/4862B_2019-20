8445a6ba358d5c14cc7b304a5201fa10a8fdee70
elw773 <45023245+elw773@users.noreply.github.com>
2019-09-02 22:37:15 -0400

    Updated basic tele op
    Added togleable position-based control for the tray and position hold for the lift. The lift pushes down when at bottom and the tray has a deadband at the bottom where no force is applied to reduce strain on the motor when instaking cubes.

--- .vscode/cquery_cached_index/c@@users@elw77@github@4862b_2019-20/src@opcontrol.cpp
+++ .vscode/cquery_cached_index/c@@users@elw77@github@4862b_2019-20/src@opcontrol.cpp
@@ -1,5 +1,11 @@
 #include "main.h"
 
+int TILT_DOWN = 200;
+int TILT_MID = 1000;
+int TILT_UP = 4300;
+
+int LIFT_DOWN = 100;
+
 /**
  * Runs the operator control code. This function will be started in its own task
  * with the default priority and stack size whenever the robot is enabled via
@@ -17,8 +23,6 @@
  	pros::Controller master(pros::E_CONTROLLER_MASTER);
    pros::lcd::initialize();
 
- 	int liftPosition = 2;
- 	int trayPosition = 0;
    int left, right;
 
    pros::Motor left1(1, MOTOR_GEARSET_06, true);
@@ -27,12 +31,16 @@
    pros::Motor right2(4,MOTOR_GEARSET_06, true);
    pros::Motor tilter(8, MOTOR_GEARSET_36, false);
    pros::Motor lift(5, MOTOR_GEARSET_36, false);
-   pros::Motor intake1(6, MOTOR_GEARSET_18, true);
-   pros::Motor intake2(7, MOTOR_GEARSET_18, false);
+   pros::Motor intake1(6, MOTOR_GEARSET_18, false);
+   pros::Motor intake2(7, MOTOR_GEARSET_18, true);
+
+  bool tilt_pressed = false;
+  int tilt_goal = 0;
 
  	while (true) {
+     pros::lcd::print(0, "Lift: %f", lift.get_position());
+     pros::lcd::print(1, "Tilt: %f", tilter.get_position());
 
-     pros::lcd::print(0, "Buttons Bitmap: %d\n", pros::lcd::read_buttons());
 
      left = master.get_analog(ANALOG_RIGHT_Y) + master.get_analog(ANALOG_RIGHT_X);
      right = master.get_analog(ANALOG_RIGHT_Y) - master.get_analog(ANALOG_RIGHT_X);
@@ -44,8 +52,8 @@
 
  		if(master.get_digital(DIGITAL_R1)){
  			if(master.get_digital(DIGITAL_R2)){
- 				intake1.move(-60);
- 				intake2.move(-60);
+ 				intake1.move(-40);
+ 				intake2.move(-40);
  			}else{
  				intake1.move(100);
  				intake2.move(100);
@@ -58,13 +66,55 @@
  			intake2.move(0);
  		}
 
-   	tilter.move(master.get_analog(ANALOG_LEFT_Y));
+    if (lift.get_position() < LIFT_DOWN && master.get_analog(ANALOG_LEFT_Y) < 2) {
+      lift.move(-4);
+    } else {
+      lift.set_brake_mode(MOTOR_BRAKE_HOLD);
+      lift.move(master.get_analog(ANALOG_LEFT_Y));
+    }
 
- 		if(master.get_digital(DIGITAL_L2)){
- 			lift.move(60);
- 		} else if(master.get_digital(DIGITAL_L1)){
- 			lift.move(-10);
- 		}
+    if(!(master.get_digital(DIGITAL_L1) || master.get_digital(DIGITAL_L2))){
+      tilt_pressed = false;
+    }
+
+    int delta = 0;
+    if(!tilt_pressed){
+      if(master.get_digital(DIGITAL_L1)){
+        tilt_pressed = true;
+        delta = 1;
+      } else if(master.get_digital(DIGITAL_L2)){
+        tilt_pressed = true;
+        delta = -1;
+      }
+    }
+    tilt_goal += delta;
+
+    switch(tilt_goal){
+      case -1:
+        tilt_goal = 0;
+        break;
+      case 0:
+        tilter.move_absolute(0, 60);
+        if(tilter.get_position() < TILT_DOWN){
+          tilter.move(0);
+        }
+        break;
+      case 1:
+        tilter.move_absolute(TILT_MID, 60);
+        break;
+      case 2:
+        tilter.move_absolute(TILT_UP, 25);
+        lift.move(-4);
+        break;
+      case 3:
+        tilt_goal = 2;
+        break;
+    }
+
+
+
+
+
 
  		pros::delay(20);
  	}
